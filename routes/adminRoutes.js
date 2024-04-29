5e41c7bd3580290ad5177a9d85d489b9a0c00905
TENSAEA <tensaeaschalew26@gmail.com>
2024-04-29 11:18:52 +0300

    remove admin dashboard route from admin route

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const adminController = require("../controllers/adminController");
 
 // Admin-specific routes
-router.get("/dashboard", adminController.getAdminDashboard);
 router.post("/create-user", adminController.createUser);
 router.put("/update-user/:id", adminController.updateUser);
 router.delete("/delete-user/:id", adminController.deleteUser);
